85b28116c6535f47ee0711ba897ff06600fa51a6
手嶋守 <tejima@gmail.com>
2010-05-12 21:50:18 +0900

    Done

--- apps/pc_backend/modules/opLunchRandomizerPlugin/lib/LunchRandomizerPluginConfigForm.class.php
+++ apps/pc_backend/modules/opLunchRandomizerPlugin/lib/LunchRandomizerPluginConfigForm.class.php
@@ -3,6 +3,7 @@ class LunchRandomizerPluginConfigForm extends sfForm
 {
   protected $configs = array(
     'lr_body' => 'oplunchrandomizerplugin_lr_body',
+    'lr_footer' => 'oplunchrandomizerplugin_lr_footer',
     'lr_from' => 'oplunchrandomizerplugin_lr_from',
     'lr_community' => 'oplunchrandomizerplugin_lr_community',
   );
@@ -10,18 +11,21 @@ class LunchRandomizerPluginConfigForm extends sfForm
   {
     $this->setWidgets(array(
       'lr_body' => new sfWidgetFormTextArea(),
+      'lr_footer' => new sfWidgetFormTextArea(),
       'lr_from' => new sfWidgetFormInput(),
       'lr_community' => new sfWidgetFormInput(),
     ));
     $this->setValidators(array(
       'lr_body' => new sfValidatorString(array(),array()),
+      'lr_footer' => new sfValidatorString(array(),array()),
       'lr_from' => new sfValidatorString(array(),array()),
       'lr_community' => new sfValidatorString(array(),array()),
     ));
 
     $this->widgetSchema->setHelp('lr_body', 'イベント本文');
-    $this->widgetSchema->setHelp('lr_from', 'イベント作成者のメンバーID');
-    $this->widgetSchema->setHelp('lr_community', 'コミュニティID');
+    $this->widgetSchema->setHelp('lr_footer', 'イベントフッター文章');
+    $this->widgetSchema->setHelp('lr_from', 'イベント作成者(エージェント)のメンバーID');
+    $this->widgetSchema->setHelp('lr_community', '対象のコミュニティID');
 
     foreach($this->configs as $k => $v)
     {
--- lib/task/LunchTask.class.php
+++ lib/task/LunchTask.class.php
@@ -11,11 +11,166 @@ class LunchEventTask extends sfBaseTask
     $this->name      = 'LunchEvent';
     $this->aliases   = array('sa-lunch');
     $this->breafDescription = '';
+
+    $this->addArgument('mode', null , sfCommandOption::PARAMETER_REQUIRED, 'mode');
   }
   protected function execute($arguments = array(),$options = array())
   {
     $databaseManager = new sfDatabaseManager($this->configuration);
-    $this->test_createevent();
+    if($arguments['mode'] == 'create'){
+      $this->test_createevent();
+    } else if($arguments['mode'] == 'shuffle') {
+      $this->shuffle();
+    }
+  }
+  private function test_getmember(){
+    $member = Doctrine::getTable('Member')->find(1);
+    print_r($member);
+  }
+  private function shuffle(){
+    $alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $target_event_id = Doctrine::getTable('SnsConfig')->get('target_lunch_event');
+
+    if(!$target_event_id){
+      echo "no target event";
+      return false;
+    }
+
+    $eMembersList = Doctrine::getTable('CommunityEventMember')->findByCommunityEventId($target_event_id);
+    $members = array();
+    foreach($eMembersList as $member){
+      $m = Doctrine::getTable('Member')->find($member->member_id);
+      $members[$member->member_id]['nickname'] = $m->name;
+    }
+
+    //グループ数の目安
+    $group_len = floor((count($members) - 2) / 4);
+    
+    //対象イベントのコメント。参加希望を取得する
+    $commentList = Doctrine::getTable('CommunityEventComment')->findByCommunityEventId($target_event_id);
+    foreach($commentList as $comment){
+      $body = $comment->getBody();
+      $body = strtoupper(mb_convert_kana($body,'a','UTF8'));
+      $body = preg_replace('/で|希望/','陣',$body);
+      $body = preg_replace('/～|~|から|以降/','-',$body);
+      $body = preg_replace('/([A-E])(まで|より前|以前)/','-$1',$body);
+      if (preg_match('/([A-E])--?([A-E]?)/',$body,$match))
+      {
+        $min = strpos($alphabet,$match[1]);
+        $max = 3;
+        if ($match[2])
+        {
+          $max = strpos($alphabet,$match[2]);
+        }
+        if ($group_len < 3)
+        {
+          $max = $group_len;
+        }
+        $r = rand($min,$max);
+        $body = preg_replace('/[A-E]--?[A-E]?/',substr($alphabet,$r,1)."陣",$body);
+      }
+      else if(preg_match('/-([A-E])/',$body,$match))
+      {
+        $max = strpos($alphabet,$match[1]);
+        if ($group_len < 3)
+        {
+          $max = $group_len;
+        }
+        $r = rand(0,$max);
+        $body = preg_replace('/-[A-E]/',substr($alphabet,$r,1)."陣",$body);
+      }
+      if (preg_match('/([A-E])陣/',$body,$match) && isset($members[$comment->getMemberId()]))
+      {
+        $members[$comment->getMemberId()]['h'] = $match[1];
+      }
+      if (preg_match('/(X|別)陣/',$body,$match) && isset($members[$comment->getMemberId()]))
+      {
+        $members[$comment->getMemberId()]['h'] = 'X';
+      }
+    }
+    //シャッフル
+    shuffle($members);
+    $groups = array();
+    //希望あり優先決定
+    foreach ($members as $i => $member)
+    {
+      if (@$member['h'])
+      {
+        $h = strpos($alphabet,$member['h']);
+        $groups[$h][] = $member;
+        unset($members[$i]);
+      }
+    }
+    $p = 0;
+    foreach ($members as $member)
+    {
+      if (count(@$groups[$p]) >= 4)
+        $p++;
+      $groups[$p][] = $member;
+      if (count(@$groups[$p]) >= 4)
+        $p++;
+    }
+
+    //端数処理
+    foreach ($groups as $i => $group)
+    {
+      if (count($group) <= 2)
+      {
+        $j = $i - 1;
+        while (true)
+        {
+          if ($j < 0 || !@$groups[$j])
+          {
+            break;
+          }
+          switch (count($groups[$i]))
+          {
+            case 1:
+              if (count($groups[$j]) >= 4)
+              {
+                $groups[$j][] = array_pop($groups[$i]);
+                unset($groups[$i]);
+                break 2;
+              }
+              break;
+            case 2:
+              if (count($groups[$j]) >= 4)
+              {
+                $groups[$j][] = array_pop($groups[$i]);
+                break;
+              }
+              break;
+          }
+          $j--;
+        }
+      }
+    }
+    ksort($groups);
+    //コメントとしてシャッフル結果を発表
+    $result = "";
+    foreach ($groups as $i => $group)
+    {
+      $result .= substr($alphabet,$i,1) . "\n";
+      foreach ($group as $j => $member)
+      {
+        $result .= $member['nickname'] . "\n";
+      }
+    }
+    $result .= "\n\n";
+    $result .= Doctrine::getTable('SnsConfig')->get('oplunchrandomizerplugin_lr_footer','');
+
+    //シャッフル結果をコメントに書き込む
+    $comment = new CommunityEventComment();
+    $comment->setCommunityEventId($target_event_id);
+    $comment->setMemberId(Doctrine::getTable('SnsConfig')->get('oplunchrandomizerplugin_lr_from',1));
+    $comment->setBody($result);
+    $comment->save();
+    Doctrine::getTable('SnsConfig')->set('target_lunch_event',null);
+
+    $this->log2activity(Doctrine::getTable('SnsConfig')->get('oplunchrandomizerplugin_lr_from',1),'ランチメンバーシャッフル完了！');
+  }
+  private function kibouList4text($text){
+    
   }
   private function test_createevent(){
     $event_date = strtotime('tomorrow');
@@ -35,5 +190,18 @@ class LunchEventTask extends sfBaseTask
     $event->setBody($body);
     $event->setOpenDate(date("Y-m-d",$event_date));
     $event->save();
+
+    $member = Doctrine::getTable('Member')->find($member_id);
+
+    Doctrine::getTable('SnsConfig')->set('target_lunch_event',$event->id);
+
+    $this->log2activity($member_id,'ランチイベントイベントを作成！');
+  }
+  private function log2activity($id,$body){
+    $act = new ActivityData();
+    $act->setMemberId($id);
+    $act->setBody($body);
+    $act->setIsMobile(0);
+    $act->save();
   }
 }
